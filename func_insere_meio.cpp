946208652bd0612b273b10d2958c52833ea4c534
DanielMonteiroo <danieel.ios90@gmail.com>
2023-09-12 19:45:42 -0300

    Arquivos da instrução prática 11

--- func_insere_meio.cpp
+++ func_insere_meio.cpp
@@ -3,3 +3,47 @@
 #include <stdlib.h>
 using namespace std;
 
+void insere_meio(int vet[], int *tam, int elemento) {
+    
+    if (*tam >= 2)
+    {
+        
+        int meio = *tam / 2;
+        
+        
+        for (int i = *tam - 1; i >= meio; i--)
+        {
+            vet[i + 1] = vet[i];
+        }
+        
+      
+        vet[meio] = elemento;
+        
+        
+        (*tam)++;
+    } 
+    else
+    {
+      
+        cout<<"O vetor deve ter pelo menos 2 elementos para inserção no meio."<<endl;
+    }
+}
+
+int main()
+ {
+    int vetor[7] = {1, 2, 3, 4, 5, 6};
+    int qtde = 6;
+    
+    int elemento = 100;
+    
+    insere_meio(vetor, &qtde, elemento);
+    
+    cout<<"Vetor resultante: "<<endl;
+    for (int i = 0; i < qtde; i++) 
+    {
+        cout<<" "<<vetor[i];
+    }
+    cout<<""<<endl;
+    
+    return 0;
+}
